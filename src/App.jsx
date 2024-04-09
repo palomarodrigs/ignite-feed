c454d5ac58421e091bce5d03692a8f233f46f452
Paloma Rodrigues <palomarodrigues.0013@gmail.com>
2024-04-09 19:58:29 -0300

    feat: add sidebar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,20 @@
+import { Header } from './components/header'
+
+import styles from './App.module.css'
+
 import './global.css'
+import { Sidebar } from './components/sidebar'
 
 export function App() {
-  return <h1>Hello World</h1>
+  return (
+    <div>
+      <Header />
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+
+        <main></main>
+      </div>
+    </div>
+  )
 }
---
+++ src/components/sidebar.jsx
@@ -0,0 +1,28 @@
+import { PencilLine } from 'phosphor-react'
+
+import styles from './sidebar.module.css'
+
+export function Sidebar() {
+  return (
+    <aside className={styles.sidebar}>
+      <img
+        className={styles.cover}
+        src="https://images.unsplash.com/photo-1587620962725-abab7fe55159?q=50&w=500&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+      />
+
+      <div className={styles.profile}>
+        <img className={styles.avatar} src="https://github.com/palomarodrigs.png" />
+
+        <strong>Paloma Rodrigues</strong>
+        <span>Front-end Developer</span>
+      </div>
+
+      <footer>
+        <a href="#">
+          <PencilLine size={20} />
+          Edit your profile
+        </a>
+      </footer>
+    </aside>
+  )
+}
