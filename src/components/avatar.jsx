2ff0511c3b859465cfdb6d0c1654dd7b0b7f5581
Paloma Rodrigues <palomarodrigues.0013@gmail.com>
2024-04-09 23:13:49 -0300

    feat: add avatar component

---
+++ src/components/avatar.jsx
@@ -0,0 +1,5 @@
+import styles from './avatar.module.css'
+
+export function Avatar({ hasBorder = true, src }) {
+  return <img className={hasBorder ? styles.avatarWithBorder : styles.avatar} src={src} />
+}
