8d9c9c2e24ffefe0fff3d85da9ffe082e69d21df
palomarodrigs <palomarodrigues.0013@gmail.com>
2024-06-26 20:54:58 -0300

    feat: add array posts to App and map props to Post component

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,55 @@ import styles from './App.module.css'
 
 import './global.css'
 
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://github.com/palomarodrigs.png',
+      name: 'Paloma Rodrigues',
+      role: 'Software Developer',
+    },
+    content: [
+      { type: 'paragraph', content: 'Hi guys 👋' },
+      {
+        type: 'paragraph',
+        content:
+          'I just uploaded a new project on my github. It’s a project I did with ai, The project name is easyup 🚀',
+      },
+      {
+        type: 'paragraph',
+        content:
+          'It’s a simple web app that allows you to upload a file and get a link to share it.',
+      },
+      { type: 'link', content: '👉 palomarodrigs/easyup' },
+    ],
+    publishedAt: new Date('2024-06-25 20:00:00'),
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'https://github.com/maykbrito.png',
+      name: 'Mayk Brito',
+      role: 'Educator @Rocketseat',
+    },
+    content: [
+      { type: 'paragraph', content: 'Hey devs 👋' },
+      {
+        type: 'paragraph',
+        content:
+          'I just released a new project on my github, the project name is FocusTimer 🚀',
+      },
+      {
+        type: 'paragraph',
+        content:
+          'FocusTimer its a timer that uses the Pomodoro technique to increase productivity by dividing work into 25 minutes intervals with short breaks to maintain focus and efficiency.',
+      },
+      { type: 'link', content: '👉 palomarodrigs/focustimer' },
+    ],
+    publishedAt: new Date('2024-06-30 20:00:00'),
+  },
+]
+
 export function App() {
   return (
     <div>
@@ -15,7 +64,14 @@ export function App() {
         <Sidebar />
 
         <main>
-          <Post />
+          {posts.map((post) => (
+            <Post
+              key={post.id}
+              author={post.author}
+              content={post.content}
+              publishedAt={post.publishedAt}
+            />
+          ))}
         </main>
       </div>
     </div>
